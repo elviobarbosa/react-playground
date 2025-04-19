e4897daa88ad0026f8d8da7fc9436e9c7a40e02f
Elvio Barbosa <elviobarbosa@gmail.com>
2025-04-19 18:08:39 -0300

    delete: removendo desnecessarios

--- src/shared/lib/helpers/update-field.ts
+++
@@ -1,6 +0,0 @@
-export const updateField = (index: number, campo: unknown, valor: unknown) => {  
-  console.log(valor.value)
-  // const novosEnderecos = [...enderecos];  
-  // novosEnderecos[index][campo] = valor;  
-  // setEnderecos(novosEnderecos);  
-};  
