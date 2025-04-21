ae46f0c34e2404608b265dbf692d2c5679b69528
Elvio Barbosa <elviobarbosa@gmail.com>
2025-04-21 20:21:46 -0300

    feat: incluindo lazy

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -1,10 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MainLayout from "./MainLayout";
-import AboutProjectComponent from "./features/about/components/about-project";
-import AboutElvioComponent from "./features/about/components/about-elvio";
-import RegisterUserNewComponent from "./features/register-user/container/register-user-new";
-import MoviesIndexComponent from "./features/movies/container/movies-index";
+
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { lazy } from "react";
+const MoviesIndexComponent = lazy(() => import("./features/movies/container/movies-index"));
+const AboutProjectComponent = lazy(() => import("./features/about/components/about-project"));
+const AboutElvioComponent  = lazy(() => import("./features/about/components/about-elvio"));
+const RegisterUserNewComponent =  lazy(() => import("./features/register-user/container/register-user-new"));
 
 function AppRoutes() {
   const queryClient = new QueryClient();
